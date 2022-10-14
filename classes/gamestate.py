8478e10d15c304c4cea9fb2acd0c94f60925cb20
J.J. Montgomery <jmonty42.dev@gmail.com>
2022-10-13 17:14:50 -0700

    Will now pickle the game state and load from that if present

--- classes/gamestate.py
+++ classes/gamestate.py
@@ -1,6 +1,7 @@
-import re
 import flint
 import os
+import pickle
+import re
 
 from classes.base import Base
 from classes.commodity import Commodity
@@ -8,6 +9,7 @@ from classes.system import System
 
 # This is found in the EXE\flhook_plugins\ folder
 OVERRIDE_FILE = "prices.cfg"
+PICKLE_FILE = "gamestate.pickle"
 
 
 class GameState(object):
@@ -21,8 +23,23 @@ class GameState(object):
         self.bases = {}
         self.commodities = {}
 
-        flint.set_install_path(os.environ['FREELANCER_FOLDER'])
+        try:
+            with open(PICKLE_FILE, 'rb') as infile:
+                game = pickle.load(infile)
+            print("Loading game state from file.")
+            self.systems = game.systems
+            self.bases = game.bases
+            self.commodities = game.commodities
+        except Exception:
+            flint.set_install_path(os.environ['FREELANCER_FOLDER'])
 
+            self.get_systems()
+            self.get_commodities()
+            self.process_override_file()
+            self.best_price_for_each_commodity()
+            self.save_to_file()
+
+    def get_systems(self):
         # First read in the names of the systems
         flint_systems: flint.routines.EntitySet[flint.routines.System] = flint.routines.get_systems()
 
@@ -34,34 +51,38 @@ class GameState(object):
                 system_id=system_id,
                 name=flint_system.name()
             )
-            base: flint.routines.BaseSolar
-            for base in flint_system.bases():
-                system_id = flint_system.nickname.lower()
-                base_id = base.base.lower()
-                if not base_id.startswith(system_id):
-                    # two bases currently have mismatched base names, which is annoying
-                    match base_id:
-                        case "br04_01_base":
-                            base_id = "bw01_01_base"
-                        case "rh10_02_base":
-                            base_id = "rh01_01_base"
-                self.systems[system_id].bases.add(base_id)
-                self.bases[base_id] = Base(
-                    name=base.name(),
-                    base_id=base_id,
-                    system=system_id
-                )
-                buying_commodities = base.universe_base().buys()
-                for commodity in buying_commodities:
-                    commodity_id = commodity.nickname.lower()
-                    self.bases[base_id].commodity_prices[commodity_id] = buying_commodities[commodity]
-                selling_commodities = base.universe_base().sells()
-                for commodity in selling_commodities:
-                    commodity_id = commodity.nickname.lower()
-                    self.bases[base_id].commodity_prices[commodity_id] = selling_commodities[commodity]
-                    self.bases[base_id].commodities_to_buy.add(commodity_id)
+            self.add_bases_in_system(flint_system)
 
-        # Next read in the names of the commodities
+    def add_bases_in_system(self, flint_system: flint.routines.System):
+        base: flint.routines.BaseSolar
+        for base in flint_system.bases():
+            system_id = flint_system.nickname.lower()
+            base_id = base.base.lower()
+            if not base_id.startswith(system_id):
+                # two bases currently have mismatched base names, which is annoying
+                match base_id:
+                    case "br04_01_base":
+                        base_id = "bw01_01_base"
+                    case "rh10_02_base":
+                        base_id = "rh01_01_base"
+            self.systems[system_id].bases.add(base_id)
+            self.bases[base_id] = Base(
+                name=base.name(),
+                base_id=base_id,
+                system=system_id
+            )
+            buying_commodities = base.universe_base().buys()
+            for commodity in buying_commodities:
+                commodity_id = commodity.nickname.lower()
+                self.bases[base_id].commodity_prices[commodity_id] = buying_commodities[commodity]
+            selling_commodities = base.universe_base().sells()
+            for commodity in selling_commodities:
+                commodity_id = commodity.nickname.lower()
+                self.bases[base_id].commodity_prices[commodity_id] = selling_commodities[commodity]
+                self.bases[base_id].commodities_to_buy.add(commodity_id)
+
+    def get_commodities(self):
+        # read in the names of the commodities
         flint_commodities = flint.get_commodities()
 
         for fl_commodity in flint_commodities:
@@ -69,6 +90,7 @@ class GameState(object):
             new_commodity.volume = int(fl_commodity.volume)
             self.commodities[fl_commodity.nickname.lower()] = new_commodity
 
+    def process_override_file(self):
         # process the override file
         with open(OVERRIDE_FILE, 'r') as override_file:
             override_lines = override_file.readlines()
@@ -102,6 +124,7 @@ class GameState(object):
             elif "MarketGood" in line and ";Market" not in line:
                 print("ERROR: line in override file should have matched but didn't: " + line)
 
+    def best_price_for_each_commodity(self):
         # calculate the best prices for each commodity
         for commodity_id in self.commodities:
             for base_id in self.bases:
@@ -114,3 +137,7 @@ class GameState(object):
                     self.commodities[commodity_id].best_sell_prices.add_price(price, base_id)
                     if commodity_id in self.bases[base_id].commodities_to_buy:
                         self.commodities[commodity_id].best_buy_prices.add_price(price, base_id)
+
+    def save_to_file(self):
+        with open(PICKLE_FILE, 'wb') as outfile:
+            pickle.dump(self, outfile)
